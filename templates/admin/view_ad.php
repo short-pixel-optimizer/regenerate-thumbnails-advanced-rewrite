a19fcd5a13ebe2c3abd43a2caf3f0e5de4cc330c
Bas Schuiling <bas@weblogmechanic.com>
2020-05-25 14:47:07 +0000

    Problem -> Solution

--- templates/admin/view_ad.php
+++ templates/admin/view_ad.php
@@ -6,7 +6,7 @@ namespace ReThumbAdvanced;
 
 <div class='img-wrapper'><img src="<?php echo $this->getURL('images/robo-winky.png') ?>"></div>
 <div class='text-wrapper'>
-  <p><?php _e('ARE YOU CONCERNED WITH YOUR SITE SPEED? Allow ShortPixel specialists to find the problem for you.', 'regenerate-thumbnails-advanced'); ?> </p>
+  <p><?php _e('ARE YOU CONCERNED WITH YOUR SITE SPEED? Allow ShortPixel specialists to find the solution for you.', 'regenerate-thumbnails-advanced'); ?> </p>
   <p><a href="https://shortpixel.com/lp/wso/?utm_source=RTA" target="_blank"><?php _e('Find Out More', 'regenerate-thumbnails-advanced'); ?></a></p>
 </div>
 </section>
